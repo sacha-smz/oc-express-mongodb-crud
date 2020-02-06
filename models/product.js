b8cbfd52a6520409297eae89bf27460fd520c5e5
sacha-vto <43008978+sacha-vto@users.noreply.github.com>
2020-02-06 18:20:45 +0100

    Modèle Product : fix nommage du schema

--- models/product.js
+++ models/product.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
-const thingSchema = mongoose.Schema({
+const productSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
   inStock: { type: Boolean, required: true }
 });
 
-module.exports = mongoose.model("Product", thingSchema);
+module.exports = mongoose.model("Product", productSchema);
